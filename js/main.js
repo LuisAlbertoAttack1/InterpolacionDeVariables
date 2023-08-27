f5ccfc12078568edb724606352103463fe411d7f
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-08-27 14:49:43 -0600

    Nuevo Proyecto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let valor1 = "Hola";
+let valor2 = "Hola2";
+let Resultado = `Ingresamos El Texto Que Deseamos Interpolar ${valor1} El Siguiente Texto ${valor2}`;
+//COMO OBSERVAMOS TODO ES COMPLETAMENTE DIFERENTE AQUI OCUPAMOS OTRO TIPO DE COMILLAS Y IGUALMENTE EL ( $ ) Y LOS { }
+// PARA LAS VARIABLES QUE VAMOS A OCUPAR
+//      AHORA LO MANDAREMOS A IMPRIMIR Y PODREMOS OBSERVAR EL RESULTADO EN LA CONSOLA DE NUESTRO NAVEGADOR
+console.log(Resultado);
+//Veremos si nos nuestra los 2 Hola que esta guardados en las variables que creamos de tipo ( let )
